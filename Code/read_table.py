fed346b83aeae4bcbd437437f97663cb6d7fcbd1
Patrick Polanco <patapolpersonal@gmail.com>
2023-01-06 11:51:32 -0800

    Refactoring

--- Code/read_table.py
+++ Code/read_table.py
@@ -1,19 +1,24 @@
-#read_table.py implements a pandas dataframe that reads a selected d100 or d20 table from an excel file or sql database
-#Should work universally, regardless of implementation.
-#intialization
+# read_table.py implements a pandas dataframe that reads a selected d100 or d20 table from an excel file or sql database
+# Should work universally, regardless of implementation.
+# intialization
 import discord
 import numpy as np
 import pandas as pd
 import random as rand
 
-#Constuctor def
+
+# Constuctor def
 def read_table():
-	print("Module Active")
+    print("Module Active")
 
-#Wilderness Combat Encounter Table
+
+# Wilderness Combat Encounter Table
 def wild_combat():
-	d20 = rand.randint(0,19)
-	tab = pd.read_csv("wilderness_combat.csv") #Wilderness encounter table from excel to DataFrame
-	print(tab)
+    d20 = rand.randint(0, 19)
 
-wild_combat() #Testing to see if this function works as intended
+    # Wilderness encounter table from excel to DataFrame
+    tab = pd.read_csv("wilderness_combat.csv")
+    print(tab)
+
+# Testing to see if this function works as intended
+wild_combat()
