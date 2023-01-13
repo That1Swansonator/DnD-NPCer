1f82f82ce629967cd283bfd6a5552c04803b2f95
Patrick Polanco <patapolpersonal@gmail.com>
2023-01-13 11:30:00 -0800

    The Bot now responding to select words in a server chat. I finnaly figured out the correct discord functions that will print a statement in chat.
    The Next step is to get the program to connect to the read_table.py program in print the results in chat.

--- Code/bot.py
+++ Code/bot.py
@@ -1,52 +1,32 @@
 import discord
 import os
-import random as rand
-import read_table as tab
+# import read_table as tab
 from dotenv import load_dotenv
 
-#initialization Code
-#imports the Token from the TOKEN.env file (not included in the GitHub Repository)
+# initialization Code
+# imports the Token from the TOKEN.env file (not included in the GitHub Repository)
 load_dotenv()
 token = os.getenv('TOKEN')
 
-#Intializes the Intents function and disables discord features such as "<user> is typing" for performance
-intents = discord.Intents.default()
-intents.typing = False
-intents.presences = False
-intents.messages = True
-intents.guilds = True
-
-client = discord.Client(intents=intents)
+# Initializes the Intents function with all intents and sets the command prefix
+intents = discord.Intents.all()
+client = discord.Client(command_prefix="!", intents=intents)
 
 
 @client.event
 async def on_ready():
 	print("Logged in as a bot {0.user}".format(client))
 
-#client.run(token)
 
-#Bot Response Code
+# Bot Response Code
 @client.event
 async def on_message(message):
-	username = str(message.author).split("#")[0]
-	channel = str(message.author.name)
-	user_message = str(message.content)
-	print(f'Message {user_message} by {username} on {channel}')
-
 	if message.author == client.user:
 		return
 
-	if channel == "random":
-		if message.author.lower() == "hello" or message.author.lower() == 'hi':
-			await message.channel.send(f'Hello {username}')
-			return
-		
-		elif user_message.lower() == "bye":
-			await message.channel.send(f'Bye {username}')
+	if message.content.startswith('hi'):
+		await message.channel.send('Hello!')
 
-		elif user_message.lower() == "tell me a joke":
-			jokes = [" Can someone please shed more light on how my lamp got stolen?", 
-					 "Why is she called llene? She stands on equal legs.","What do you call a gazelle in a lions territory? Denzel."]
-			await message.channel.send(rand.choices(jokes))
 
+# Runs Code
 client.run(token)
